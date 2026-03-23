f84083bd992068cdca4d3ee2e1156a508c9cf5ea
YUKI ENDO <stb5181@gmail.com>
2026-03-23 23:30:57 +0900

    Fix factory email uniqueness and tailwind in test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { "test@example.com" }
+    sequence(:email) { |n| "test#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
     display_name { "テストユーザー" }
